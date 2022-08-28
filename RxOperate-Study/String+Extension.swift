77ae797e018e021da467e95386865d43116856bd
500beckwon <kjsx60@gmail.com>
2022-08-28 19:54:18 +0900

    String 값이 숫자인지 파악하는 연산 프로퍼티 추가

--- RxOperate-Study/String+Extension.swift
+++ RxOperate-Study/String+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension String {
+    var isNumber: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[0-9]+$", options: .caseInsensitive)
+            if let _ = regex.firstMatch(in: self, options: .reportCompletion, range: NSMakeRange(0, count)) { return true }
+        } catch { return false }
+        return false
+    }
+}
